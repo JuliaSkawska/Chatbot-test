2c46d1558451e6dbcba5ecb275dc059766efebbe
JuliaSkawska <101816101+JuliaSkawska@users.noreply.github.com>
2024-04-24 12:47:52 +0200

    Update main.py
    Added the start of GUI

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from tkinter import *
+class ChatInterface:
+    def __init__(self):
+        self.root = Tk()
+        self.root.resizable(height=False, width=False)
+        self.root.title('Pomoc Klienta')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        self.chat_history = Text(self.root, height=20, width=50)
+        self.chat_history.pack()
+        self.print_to_chat_history("Bot: Cześć, jak mogę ci dzisiaj pomóc?")
 
+        self.input_label = Label(self.root, text='Tu napisz swoją wiadomość', fg='green')
+        self.input_label.pack()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        self.user_input = Entry(self.root)
+        self.user_input.pack()
 
+        self.user_input.bind("<Return>", self.submit_input)
+        self.root.mainloop()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def submit_input(self, event):
+        user_input = self.user_input.get()
+        self.user_input.delete(0, END)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if user_input.strip() == "":
+            self.root.quit()
+            return
+        self.print_to_chat_history(user_input)
+        bot_response = "Bot: To jest automatyczna odpowiedź."
+        self.print_to_chat_history(bot_response)
+
+    def print_to_chat_history(self, message):
+        self.chat_history.insert(END, message + "\n")
+        self.chat_history.see(END)
+
+if __name__ == "__main__":
+    ChatInterface()
